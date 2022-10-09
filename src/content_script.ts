7cbdf181e42e7bf6ade5e9cfac53bc00b289bc03
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2022-10-09 14:46:15 +0900

    clean: リファクタリング

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,19 +1,4 @@
-import SearchPage from './content_scripts/search_page';
+import { SearchPage } from './content_scripts/search_page';
 
-chrome.runtime.onMessage.addListener(() => {
-  SearchPage();
-});
-
-chrome.runtime.sendMessage('');
-
-let href = location.href;
-const observer = new MutationObserver(() => {
-  if (href !== location.href) {
-    chrome.runtime.sendMessage('');
-    href = location.href;
-  }
-});
-
-observer.observe(document, { childList: true, subtree: true });
-
+SearchPage();
 export {};
